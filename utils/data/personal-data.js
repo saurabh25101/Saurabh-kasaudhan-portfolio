77581ad1ecc1de9d9f4c919c4ef56967e2d18a06
Saurabh Kasaudhan <saurabhkasaudhan97@gmail.com>
2025-12-17 12:45:26 +0530

    Updated project files after editing locally

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+ export const personalData = {
+  name: "Saurabh Kasaudhan",
+  profile: "/profile.png",
+  designation: " Full Stack Web Developer |Frontend(React)",
+  description: "Hi  I'm Saurabh Kasaudhan , a passionate and detail-oriented Software Developer with a strong foundation in web technologies. I have deep knowledge of HTML, CSS, Bootsrap, JavaScript, React.js, Node.js, MongoDB, and RESTful APIs. I am a quick learner with a proactive approach, always eager to improve my skills and stay updated with the latest industry trends. I love building interactive, user-friendly web applications and am always open to collaborating on innovative projects. I am available for opportunities that align with my skills and interests.",
+  email: "saurabhkasaudhan97@gmail.com",
+  phone: "+91 8009233771",
+   
+  github: " https://github.com/saurabh25101",
+  facebook: "", // optional, can add later
+  linkedIn: "https://www.linkedin.com/in/saurabh-kasaudhan-338944276",
+  twitter: "", // optional
+  stackOverflow: "", // optional
+  leetcode: "", // optional
+  devUsername: "saurabhkasaudhan",
+  resume: "https://drive.google.com/file/d/1tQFKcjlunY_w8pTJ_EW4YHCNN9UDKBkK/view?usp=drivesdk" // paste your resume link here when ready (Google Drive or PDF)
+};
